09a049a23926dcc892c616752c088d808342a2d2
mitsunori.morita <mistunori.morita@speee.jp>
2017-10-16 20:53:35 +0900

    styleing styles add

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import React from 'react';
+import { Text,AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
+// import App from './App';
+
+const App = () => (
+  <Header />
+);
+
 
 AppRegistry.registerComponent('albums', () => App);
