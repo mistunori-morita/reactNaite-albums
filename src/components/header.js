79e211d96c493a521798cc06c3491876321f983e
mitsunori.morita <mistunori.morita@speee.jp>
2017-10-16 20:54:57 +0900

    header.js styling css add

---
+++ src/components/header.js
@@ -0,0 +1,25 @@
+// Improt libraries for maiking a components
+import React from 'react';
+import { Text,View } from 'react-native';
+
+
+//Make a component
+const Header = () => {
+  const { textStyle ,viewStyle } = styles;
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums!</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle:{
+    backgroundColor: '#F8F8F8'
+  },
+  textStyle:{
+    fontSize: 20
+  }
+};
+// Make the component available to other parts of the App
+export default Header;
