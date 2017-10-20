e7dccc9138f96d0dec2b409cefa6d373a4504962
mitsunori.morita <mistunori.morita@speee.jp>
2017-10-20 20:37:04 +0900

    header.js props headerText add

--- src/components/header.js
+++ src/components/header.js
@@ -4,11 +4,11 @@ import { Text,View } from 'react-native';
 
 
 //Make a component
-const Header = () => {
+const Header = (props) => {
   const { textStyle ,viewStyle } = styles;
   return (
     <View style={viewStyle}>
-      <Text style={textStyle}>Albums!</Text>
+      <Text style={textStyle}>{props.headerText}</Text>
     </View>
   );
 };
@@ -22,7 +22,9 @@ const styles = {
     paddingTop: 15,
     shadowColor: '#000',
     shadowOffset: {widt: 0, height: 2},
-    shadowOpacity: 0.2
+    shadowOpacity: 0.2,
+    elevation:2 ,
+    position: 'relative'
   },
   textStyle:{
     fontSize: 20
